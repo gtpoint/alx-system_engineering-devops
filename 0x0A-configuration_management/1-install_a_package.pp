254f83022a56912d98a4917d88605e30fb96f6d6
Oussama El aouzari <125877426+gtpoint@users.noreply.github.com>
2023-09-23 01:36:26 +0200

    Update 1-install_a_package.pp

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,12 @@
-# Installs puppet-lint
+# 1-install_a_package.pp
 
-package { 'puppet-lint':
-  ensure   => '2.1.0',
-  provider => 'gem',
+package { 'python3-pip':
+  ensure => installed,
+}
+
+exec { 'install_flask':
+  command => '/usr/bin/pip3 install Flask==2.1.0',
+  path    => ['/usr/bin', '/usr/local/bin'],
+  creates => '/usr/local/lib/python3.*/dist-packages/Flask-2.1.0-py3.*.egg-info',
+  require => Package['python3-pip'],
 }
