95818dabb672b45a0221f296878fad065ff3a3ad
Oussama El aouzari <125877426+gtpoint@users.noreply.github.com>
2023-10-24 23:14:31 +0200

    Update 1-export_to_CSV.py

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 """Exports to-do list information for a given employee ID to CSV format."""
-import csv, requests, sys
+import csv
+import requests
+import sys
 
 if __name__ == "__main__":
     user_id = sys.argv[1]
